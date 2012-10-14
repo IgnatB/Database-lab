e44d12ef7f3b8a1666434d163caadd05c82ffaa1
IgnatB <b_ignat_v@i.ua>
2012-10-15 02:56:31 +0300

    Prototype

---
+++ main.py
@@ -0,0 +1,10 @@
+from databasecore import *
+from uiofdata import *
+
+x = IU_Dbp()
+x.open_database()
+x.print_10()
+print(x.setyear1)
+print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+print(x.setyear2)
+x.IU_window()
